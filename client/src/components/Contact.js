2e95337e4ae46a6568881b07c7c1f28c3fe6b63b
Lenny Dickey <Len.dickey@protonmail.com>
2019-10-03 11:43:29 -0400

    update

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,52 +2,132 @@ import React from "react";
 import "./styles.css";
 
 class Contact extends React.Component {
-  render() {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        contact: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("/contact")
+        .then(res => res.json())
+        .then(
+          data => {
+            console.log("data.contact - ", data);
+            this.setState({
+              isLoaded: true,
+              contact: data
+            });
+          },
+          error => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+    }
+    render() {
+      const { error, isLoaded, contact } = this.state;
+
     return (
       <>
-  <div id="con">
-        <h2>Contact Us:</h2>
-        <form action="">
-          <section class="left">
-            <div class="input-container">
-              <label for="name">Name</label>
-              <input type="text" required />
+        <div id="con">
+          <h2>Contact Us:</h2>
+          <form action="">
+            <section className="left">
+              <div className="input-container">
+                <label htmlFor="name">Name</label>
+                <input type="text" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="age" required>
+                  Age
+                </label>
+                <input type="text" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="phone">Phone</label>
+                <input type="text" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="email">Email</label>
+                <input type="text" required />
+              </div>
+            </section>
+            <section className="right">
+              <div className="input-container">
+                <label htmlFor="address">Address 1</label>
+                <input type="text" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="subject">Subject</label>
+                <input type="text" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="comments">Comments</label>
+                <textarea name="comments" id="" required />
+              </div>
+            </section>
+            <div className="send-container">
+              <input type="submit" value="Send" onClick="return true" />
+              {/* <br/> */}
             </div>
-            <div class="input-container">
-              <label for="age" required>
-                Age
-              </label>
-              <input type="text" required />
-            </div>
-            <div class="input-container">
-              <label for="phone">Phone</label>
-              <input type="text" required />
-            </div>
-            <div class="input-container">
-              <label for="email">Email</label>
-              <input type="text" required />
-            </div>
-          </section>
-          <section class="right">
-            <div class="input-container">
-              <label for="address">Address 1</label>
-              <input type="text" required />
-            </div>
-            <div class="input-container">
-              <label for="subject">Subject</label>
-              <input type="text" required />
-            </div>
-            <div class="input-container">
-              <label for="comments">Comments</label>
-              <textarea name="comments" id="" required />
-            </div>
-          </section>
-          <div class="send-container">
-            <input type="submit" value="Send" onclick="return true" />
-            {/* <br/> */}
-          </div>
-        </form>
+          </form>
         </div>
+        <br></br>
+        {contact.map((contact, index) => (
+
+        <div className="container">
+          <div id="carouselContent" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner" role="listbox">
+            {/* {contact.map((contact, index) => ( */}
+
+              <div className="carousel-item active text-center p-4">
+                <p>"{contact.comment}"</p>
+                <p>
+                  <font>{contact.name}</font>
+                </p>
+              </div>
+              <div className="carousel-item text-center p-4">
+                <p>"Second quote goes here."</p>
+                <p>
+                  <font>First Last</font>
+                </p>
+              </div>
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselContent"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselContent"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        )
+        )}
       </>
     );
   }
