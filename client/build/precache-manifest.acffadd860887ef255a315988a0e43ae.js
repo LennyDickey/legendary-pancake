74fc75481ca82731b69ef3ca3b711478a8f4cfa7
Lenny Dickey <Len.dickey@protonmail.com>
2019-11-21 19:06:52 -0500

    update

---
+++ client/build/precache-manifest.acffadd860887ef255a315988a0e43ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb7c91d8b2786a2eff55f537d91d594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb92a92f0647ce211c97",
+    "url": "/static/css/main.74843e29.chunk.css"
+  },
+  {
+    "revision": "e738b5746bee3aa3b976",
+    "url": "/static/js/2.0599c823.chunk.js"
+  },
+  {
+    "revision": "fb92a92f0647ce211c97",
+    "url": "/static/js/main.2f99a946.chunk.js"
+  },
+  {
+    "revision": "0cfd9da876ec20bd38cc",
+    "url": "/static/js/runtime~main.9a21d0a4.js"
+  }
+]);
